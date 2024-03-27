41b1a9bc047a5a4db36d3804104e190f01018d43
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2024-03-27 12:30:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
         </body>
       </html>
+      <Analytics/>
     </ClerkProvider>
   )
 }
