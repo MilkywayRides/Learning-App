9ef4f63e3e64a3273f6f9378c8a408f66978246c
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2024-04-02 22:52:37 +0530

    Update navbar.tsx

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
   return (
-    <div className="p-4 border-b h-3/5 flex items-center bg-white shadow-sm">
+    <div className="p-4 border-b h-3/4 flex items-center bg-white shadow-sm">
       <MobileSidebar />
       <NavbarRoutes />
     </div>
