37eae90ab561f3857276ba145d1940eedfd2e24b
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2024-04-08 21:48:48 +0530

    Update category-form.tsx

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Course } from "@prisma/client";
+import React from "react";
 
 import {
   Form,
